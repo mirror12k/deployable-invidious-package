5aad5031bf2e96050e6b96012de3c62ceb3b3ffd
mirror12k <mirror12k@gmail.com>
2025-11-02 21:08:50 -0500

    - proxying

--- invidious/run.sh
+++ invidious/run.sh
@@ -5,20 +5,29 @@ echo "[i] cloning invidious git"
 tmp_dir=$(mktemp -d)
 git clone https://github.com/iv-org/invidious.git "$tmp_dir/invidious"
 
-DOMAIN="example.org"
+DOMAIN="${DOMAIN:-example.org}"
+MATERIALIOUS_URL="${MATERIALIOUS_URL:-http://localhost:3001}"
 RANDOM_HMAC_KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 64)
 RANDOM_COMPANION_KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
-echo "[i] preparing docker-compose ($VISITOR_DATA / $PO_TOKEN)..."
+
+echo "[i] preparing docker-compose..."
 cat docker-compose.yml.template \
 	| sed "s#{my_hmac_key}#$RANDOM_HMAC_KEY#g" \
 	| sed "s#{my_companion_key}#$RANDOM_COMPANION_KEY#g" \
 	| sed "s#{domain}#$DOMAIN#g" \
 	> "$tmp_dir/invidious/docker-compose.yml"
 
+echo "[i] preparing Caddyfile with MATERIALIOUS_URL=$MATERIALIOUS_URL..."
+cat Caddyfile.template \
+	| sed "s#{materialious_url}#$MATERIALIOUS_URL#g" \
+	> "$tmp_dir/invidious/Caddyfile"
+
 cat "$tmp_dir/invidious/docker-compose.yml"
 
 cd "$tmp_dir/invidious"
 
+echo "[i] killing any old images"
+docker-compose down
 echo "[i] pulling images"
 docker-compose pull
 echo "[i] uping invidious docker..."
