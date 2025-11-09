d7f55b9200e061486dbb257ebdd618b55ff87658
Mirror <mirror12k@gmail.com>
2025-11-09 10:30:20 -0500

    - fixing directory issues

--- invidious/run.sh
+++ invidious/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "[i] cloning invidious git"
-tmp_dir=$(mktemp -d)
-git clone https://github.com/iv-org/invidious.git "$tmp_dir/invidious"
+INVIDIOUS_ROOT="/invidious"
+INVIDIOUS_DATA="${INVIDIOUS_ROOT}/invidious_data"
+CADDY_DATA="${INVIDIOUS_ROOT}/caddy_data"
+
+echo "[i] cleaning up old data..."
+rm -rf "$INVIDIOUS_DATA"
+rm -rf "$CADDY_DATA"
+
+echo "[i] cloning invidious git to ${INVIDIOUS_DATA}..."
+git clone https://github.com/iv-org/invidious.git "$INVIDIOUS_DATA"
 
 DOMAIN="${DOMAIN:-example.org}"
 MATERIALIOUS_URL="${MATERIALIOUS_URL:-http://localhost:3001}"
@@ -11,21 +18,21 @@ RANDOM_HMAC_KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 64)
 RANDOM_COMPANION_KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
 
 echo "[i] preparing docker-compose..."
-cat docker-compose.yml.template \
+cat /app/docker-compose.yml.template \
 	| sed "s#{my_hmac_key}#$RANDOM_HMAC_KEY#g" \
 	| sed "s#{my_companion_key}#$RANDOM_COMPANION_KEY#g" \
 	| sed "s#{domain}#$DOMAIN#g" \
-	> "$tmp_dir/invidious/docker-compose.yml"
+	> "$INVIDIOUS_DATA/docker-compose.yml"
 
 echo "[i] preparing Caddyfile with MATERIALIOUS_URL=$MATERIALIOUS_URL..."
-mkdir "$tmp_dir/invidious/caddy_data"
-cat Caddyfile.template \
+mkdir -p "$CADDY_DATA"
+cat /app/Caddyfile.template \
 	| sed "s#{materialious_url}#$MATERIALIOUS_URL#g" \
-	> "$tmp_dir/invidious/caddy_data/Caddyfile"
+	> "$CADDY_DATA/Caddyfile"
 
-# cat "$tmp_dir/invidious/docker-compose.yml"
+# cat "$INVIDIOUS_DATA/docker-compose.yml"
 
-cd "$tmp_dir/invidious"
+cd "$INVIDIOUS_DATA"
 
 echo "[i] killing any old images"
 docker-compose down
